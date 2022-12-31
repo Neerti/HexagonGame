a2c374601c49f7a56fc9a2f44cb11bb5732d47dd
Neerti <Neerti@users.noreply.github.com>
2022-12-31 17:29:40 -0500

    Ports middle mouse drag to move from previous Hexagon codebase.

--- HexagonGame/Code/ECS/Systems/InputSystem.cs
+++ HexagonGame/Code/ECS/Systems/InputSystem.cs
@@ -10,6 +10,8 @@ namespace HexagonGame.ECS.Systems;
 public class InputSystem
 {
 	public KeyboardState OldKeyboardState = new KeyboardState();
+	public MouseState OldMouseState;
+	public Vector2 FixedTogglePoint;
 	
 	public void PollForInput(Game1 game, GameTime gameTime)
 	{
@@ -19,38 +21,71 @@ public class InputSystem
 
 		// Camera control.
 		// This should get spun off into a camera system later on so it can do things like easing.
-		var movementDirection = Vector2.Zero;
-		var cameraSpeed = 500f;
 		
-		// TODO: Keybinding system so people can rebind keys.
-		if (Keyboard.GetState().IsKeyDown(Keys.W))
+		// Mouse camera control.
+		// Click-drag camera movement code adapted from code written by "AlienBuchner" at https://godotengine.org/qa/46892/would-map-navigation-camera-with-middle-mouse#a52576.
+		if (IsMiddleMouseButtonJustDown())
 		{
-			movementDirection += -Vector2.UnitY;
+			Mouse.SetCursor(MouseCursor.Crosshair);
+			FixedTogglePoint = Mouse.GetState().Position.ToVector2();
 		}
 
-		else if (Keyboard.GetState().IsKeyDown(Keys.S))
+		if (IsMiddleMouseButtonJustUp())
 		{
-			movementDirection += Vector2.UnitY;
+			// Reset courser to normal shape.
+			Mouse.SetCursor(MouseCursor.Arrow);
 		}
+
+		if (Mouse.GetState().MiddleButton == ButtonState.Pressed)
+		{
+			var mousePos = Mouse.GetState().Position.ToVector2();
+			var cameraPos = game.World.PositionComponents.Get(game.World.CameraEntity).Position;
+			var newPos = new Vector2(
+				cameraPos.X - (mousePos.X - FixedTogglePoint.X),
+				cameraPos.Y - (mousePos.Y - FixedTogglePoint.Y)
+			);
+			game.World.PositionComponents.Get(game.World.CameraEntity).Position = newPos;
+			FixedTogglePoint = mousePos;
+		}
+
+		else
+		{
+			// Keyboard camera control.
+			var movementDirection = Vector2.Zero;
+			var cameraSpeed = 500f;
 		
-		if (Keyboard.GetState().IsKeyDown(Keys.A))
-		{
-			movementDirection += -Vector2.UnitX;
-		}
+			// TODO: Keybinding system so people can rebind keys.
+			if (Keyboard.GetState().IsKeyDown(Keys.W))
+			{
+				movementDirection += -Vector2.UnitY;
+			}
+
+			else if (Keyboard.GetState().IsKeyDown(Keys.S))
+			{
+				movementDirection += Vector2.UnitY;
+			}
 		
-		else if (Keyboard.GetState().IsKeyDown(Keys.D))
-		{
-			movementDirection += Vector2.UnitX;
-		}
+			if (Keyboard.GetState().IsKeyDown(Keys.A))
+			{
+				movementDirection += -Vector2.UnitX;
+			}
+		
+			else if (Keyboard.GetState().IsKeyDown(Keys.D))
+			{
+				movementDirection += Vector2.UnitX;
+			}
 		
 		
 
-		if (movementDirection != Vector2.Zero)
-		{
-			game.CameraSystem.MoveCamera(game.World, movementDirection, cameraSpeed, gameTime);
-			game.World.PositionComponents.Get(game.World.CameraEntity).Position += movementDirection * cameraSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+			if (movementDirection != Vector2.Zero)
+			{
+				game.CameraSystem.MoveCamera(game.World, movementDirection, cameraSpeed, gameTime);
+				game.World.PositionComponents.Get(game.World.CameraEntity).Position += movementDirection * cameraSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+			}
 		}
 
+
+		// Other keyboard input.
 		if (IsKeyJustDown(Keys.Space))
 		{
 			game.Paused = !game.Paused;
@@ -69,11 +104,67 @@ public class InputSystem
 		}
 
 		OldKeyboardState = Keyboard.GetState();
+		OldMouseState = Mouse.GetState();
 
 	}
 
+	/// <summary>
+	/// Tests if a key was just pressed. Will only be true once per key press, even if held down.
+	/// </summary>
+	/// <param name="key"><see cref="Keys"/> enum corresponding to the key to test for.</param>
+	/// <returns>True if the key is held down this frame, and it was not held down on the previous frame.</returns>
 	public bool IsKeyJustDown(Keys key)
 	{
 		return Keyboard.GetState().IsKeyDown(key) && OldKeyboardState.IsKeyUp(key);
 	}
+
+	/// <summary>
+	/// Tests if a key was just released. Will only be true once per key press.
+	/// </summary>
+	/// <param name="key"><see cref="Keys"/> enum corresponding to the key to test for.</param>
+	/// <returns>True if the key is currently up this frame, and it was not up on the previous frame.</returns>
+	public bool IsKeyJustUp(Keys key)
+	{
+		return Keyboard.GetState().IsKeyUp(key) && OldKeyboardState.IsKeyDown(key);
+	}
+	
+	// Sadly the Mouse class doesn't have a generic `IsMouseButton[Down|Up]` like the Keyboard class does.
+	// Thus methods for left, middle, and right mouse buttons each.
+	
+	/// <summary>
+	/// Tests if the left mouse button was just clicked. Will only be true once per click, even if held down.
+	/// </summary>
+	/// <returns>True if the left mouse button is held down this frame,
+	/// and it was not held down on the previous frame.</returns>
+	public bool IsLeftMouseButtonJustDown()
+	{
+		return Mouse.GetState().LeftButton == ButtonState.Pressed && OldMouseState.LeftButton == ButtonState.Released;
+	}
+	
+	public bool IsLeftMouseButtonJustUp()
+	{
+		return Mouse.GetState().LeftButton == ButtonState.Released && OldMouseState.LeftButton == ButtonState.Pressed;
+	}
+	
+	public bool IsMiddleMouseButtonJustDown()
+	{
+		return Mouse.GetState().MiddleButton == ButtonState.Pressed && OldMouseState.MiddleButton == ButtonState.Released;
+	}
+	
+	public bool IsMiddleMouseButtonJustUp()
+	{
+		return Mouse.GetState().MiddleButton == ButtonState.Released && OldMouseState.MiddleButton == ButtonState.Pressed;
+	}
+	
+	public bool IsRightMouseButtonJustDown()
+	{
+		return Mouse.GetState().RightButton == ButtonState.Pressed && OldMouseState.RightButton == ButtonState.Released;
+	}
+	
+	public bool IsRightMouseButtonJustUp()
+	{
+		return Mouse.GetState().RightButton == ButtonState.Released && OldMouseState.RightButton == ButtonState.Pressed;
+	}
+	
+
 }

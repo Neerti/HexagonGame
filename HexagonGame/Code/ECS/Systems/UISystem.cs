ca444d33fd11260f4a9c643ac26472e982795804
Neerti <Neerti@users.noreply.github.com>
2023-03-14 22:41:38 -0400

    Improves new Dear ImGui-ified debug UI.

--- HexagonGame/Code/ECS/Systems/UISystem.cs
+++ HexagonGame/Code/ECS/Systems/UISystem.cs
@@ -29,7 +29,11 @@ public class UISystem : BaseSystem<World, GameTime>
 	{
 		ImGuiRenderer.BeforeLayout(gameTime);
 		
-		ImGuiLayout();
+		
+		
+		//ImGuiLayout();
+		DebugUI();
+
 		
 		ImGuiRenderer.AfterLayout();
 		
@@ -68,48 +72,73 @@ public class UISystem : BaseSystem<World, GameTime>
 
 		SpriteBatch.End();*/
 	}
-	
-	// Direct port of the example at https://github.com/ocornut/imgui/blob/master/examples/sdl_opengl2_example/main.cpp
-	private float f = 0.0f;
 
-	private bool show_test_window = false;
-	private bool show_another_window = false;
-	private Num.Vector3 clear_color = new Num.Vector3(114f / 255f, 144f / 255f, 154f / 255f);
-	private byte[] _textBuffer = new byte[100];
-	
-	protected virtual void ImGuiLayout()
+	private bool showDemo;
+	private void DebugUI()
 	{
-		// 1. Show a simple window
-		// Tip: if we don't call ImGui.Begin()/ImGui.End() the widgets appears in a window automatically called "Debug"
+		
+		//    ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav;
+		var flags = ImGuiWindowFlags.NoDecoration | ImGuiWindowFlags.NoDocking |
+		                         ImGuiWindowFlags.AlwaysAutoResize | ImGuiWindowFlags.NoSavedSettings |
+		                         ImGuiWindowFlags.NoFocusOnAppearing | ImGuiWindowFlags.NoNav;
+		ImGui.SetNextWindowPos(new Num.Vector2(10, 10), ImGuiCond.Always);
+		ImGui.SetNextWindowBgAlpha(.35f);
+		ImGui.Begin("Debug Stats", flags);
+		ImGui.Text("Debug Window");
+		ImGui.Separator();
+		if (ImGui.Button("Show Dear ImGui demo window"))
 		{
-			ImGui.Text("Hello, world!");
-			ImGui.SliderFloat("float", ref f, 0.0f, 1.0f, string.Empty);
-			ImGui.ColorEdit3("clear color", ref clear_color);
-			if (ImGui.Button("Test Window")) show_test_window = !show_test_window;
-			if (ImGui.Button("Another Window")) show_another_window = !show_another_window;
-			ImGui.Text(string.Format("Application average {0:F3} ms/frame ({1:F1} FPS)", 1000f / ImGui.GetIO().Framerate, ImGui.GetIO().Framerate));
-
-			ImGui.InputText("Text input", _textBuffer, 100);
-
-			ImGui.Text("Texture sample");
-			//ImGui.Image(_imGuiTexture, new Num.Vector2(300, 150), Num.Vector2.Zero, Num.Vector2.One, Num.Vector4.One, Num.Vector4.One); // Here, the previously loaded texture is used
+			showDemo = !showDemo;
+			
+		}
+		if (showDemo)
+		{
+			ImGui.ShowDemoWindow();
 		}
 
-		// 2. Show another simple window, this time using an explicit Begin/End pair
-		if (show_another_window)
+		if (ImGui.CollapsingHeader("Performance"))
 		{
-			ImGui.SetNextWindowSize(new Num.Vector2(200, 100), ImGuiCond.FirstUseEver);
-			ImGui.Begin("Another Window", ref show_another_window);
-			ImGui.Text("Hello");
-			ImGui.End();
+			ImGui.Text($"{ImGui.GetIO().Framerate:F1} FPS ({1000f / ImGui.GetIO().Framerate:F3} ms/frame)");
 		}
 
-		// 3. Show the ImGui test window. Most of the sample code is in ImGui.ShowTestWindow()
-		if (show_test_window)
+		if (ImGui.CollapsingHeader("ECS"))
 		{
-			ImGui.SetNextWindowPos(new Num.Vector2(650, 20), ImGuiCond.FirstUseEver);
-			ImGui.ShowDemoWindow(ref show_test_window);
+			var entityCount = World.CountEntities(new QueryDescription().WithNone<int>());
+			ImGui.Text($"{(entityCount).ToString()} entities");
 		}
+		
+		if (ImGui.CollapsingHeader("Camera"))
+		{
+			var cameraDesc = new QueryDescription().WithExclusive<Position, Camera>();
+			World.Query(in cameraDesc, (ref Position pos, ref Camera cam) =>
+				{
+					ImGui.Text("Translation");
+					ImGui.Text($"Camera Position: ({cam.ViewPosition.X:F1}, {cam.ViewPosition.Y:F1}, {cam.ViewPosition.Z:F1})");
+					ImGui.Text("Focus Point");
+					ImGui.BeginTable("Focus Point", 3, ImGuiTableFlags.SizingStretchProp);
+					ImGui.TableNextColumn();
+					ImGui.DragFloat("X", ref pos.WorldPosition.X);
+					ImGui.TableNextColumn();
+					ImGui.DragFloat("Y", ref pos.WorldPosition.Y);
+					ImGui.TableNextColumn();
+					ImGui.DragFloat("Z", ref pos.WorldPosition.Z);
+					ImGui.EndTable();
+					
+					ImGui.Text("Rotation");
+					ImGui.SliderAngle("Azimuth Angle", ref cam.AzimuthAngle, 0f, 360f);
+					ImGui.SliderAngle("Polar Angle", ref cam.PolarAngle, 0f, 90f);
+					ImGui.Text("Zoom");
+					ImGui.SliderFloat("Radius", ref cam.Radius, 0f, 30f);
+
+				}
+			);
+		}
+
+
+
+
+
+		ImGui.End();
 	}
 }
 /*
